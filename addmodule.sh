856966f8076f78b1f534067852ac52e391125090
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2019-10-27 15:30:17 -0300

    Add script to add and init new git submodules

---
+++ addmodule.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z $1 ]; then
+    echo "Usage: ./addmodule module-name"
+    exit 1
+fi
+
+MODULE_URL="git@gitlab.com:chega.ai/$1.git"
+
+read -p "This will add and initialize $MODULE_URL as $1. Press ctrl+c to abort."
+
+set -x
+git submodule add $MODULE_URL $1
+git submodule update --init -- $1
+git add .gitmodules
+git commit -m "Add $1 as submodule"
+git push -u origin master
+set +x
