24342275e71fc9f65695e03b97f6d0b0638fdbce
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2020-01-10 23:55:16 -0300

    :white_check_mark: Add test to make sure user ID exists

--- test/src/tasks/microservices/ms-users/create-user.ts
+++ test/src/tasks/microservices/ms-users/create-user.ts
@@ -29,7 +29,11 @@ const createUser: Listr.ListrTask<GlobalContext> = {
     }
 
     const response = await ctx.http.post('/users', payload)
+
     assert(response.status === 201, `Expected ${response.status} to be 201`)
+    assert(!!response.data.user, `Expected ${response.data} to have a property 'user'`)
+    assert(!!response.data.user.id, `Expected '${JSON.stringify(response.data.user)}' to have an 'id' property`)
+
     ctx.userId = response.data.user.id
     task.title = `${task.title}: ${ctx.userId}`
   }
